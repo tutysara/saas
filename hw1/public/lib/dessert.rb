78f15b9fa85d6934b30f4a7a93adf18bf8b1e60b
tutysara <tutysara@gmail.com>
2013-12-14 17:16:24 +0530

    Completed exercise - Dessert class

--- hw1/public/lib/dessert.rb
+++ hw1/public/lib/dessert.rb
@@ -1,17 +1,35 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    @calories < 200 # less than 200 is healthy
   end
   def delicious?
-    # your code here
+    true # all desserts are delicious
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @calories = 5
+    @flavor = flavor
+    @name = flavor + " jelly bean"
+  end
+  
+  def flavor
+    @flavor
+  end
+  
+  def flavor=(flavor)
+    @flavor = flavor
+    @name = @flavor + " jelly bean"
+  end
+  
+  # overriding example
+  def delicious? 
+    return true if @flavor != "licorice"
   end
 end
